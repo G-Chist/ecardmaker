6ecefead37ed7208dc778b54bacbe30b16051a0e
G-Chist <42654606+G-Chist@users.noreply.github.com>
2023-03-03 22:01:10 +0800

    Update flask_app.py

--- mysite/flask_app.py
+++ mysite/flask_app.py
@@ -1,8 +1,6 @@
-from flask import Flask, render_template, request, redirect, url_for
-import time
-from random import *
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def main_page():
     return render_template("MainPage.html")
